b3d997f08fa318897cf40394b5ab8b7ac57a0cb8
akshaymahadik3118 <akshay.mahadik3118@gmail.com>
2026-02-26 18:19:13 +0530

    Update textToImage.py

--- Project_1/textToImage.py
+++ Project_1/textToImage.py
@@ -13,7 +13,8 @@ def textToImageFnc(prompt_data):
             {
                 "text":prompt_data,
             }
-        ]
+        ],
+        "mode": "text-to-image",
         "output_format": "png",
         "cfg_scale": 7.5,       
         "seed":80,
